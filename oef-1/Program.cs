c46710d79536384d44cbdf46aff2d9fd97b87e58
Bart Duisters <bartduisters@bartduisters.com>
2022-10-09 14:12:09 +0200

    feat: oef 1

---
+++ oef-1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Vraag aan de gebruiker wat zijn/haar naam is en sla dit op in een variabele
+
+
+
+// Vraag aan de gebruiker wat zijn/haar leeftijd is en sla dit op in een variabele
+
+
+
+// Zorg ervoor dat de gebruiker zijn/haar naam en leeftijd in de console ziet
+
+
+// Zorg ervoor dat de gebruiker zijn/haar leeftijd over 5 jaar in de console ziet
+
+
+// Let op: zorg dat er geen waarschuwingen verschijnen in de console
+
+
+/*
+* Wanneer `dotnet run Program.cs` wordt uitgevoerd, moet de volgende output verschijnen:
+*/
+
+// Wat is je naam?
+// (voorbeeld input) John Duck
+// Wat is je leeftijd?
+// (voorbeeld input) 31
+// Hallo John Duck, je bent 31 jaar oud.
+// Over 5 jaar ben je 36 jaar oud.
